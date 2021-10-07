c28683ef285cd27cbfdb13c4ae33dd6cda7aa078
Tahlil <bsse0803@iit.du.ac.bd>
2021-10-07 20:55:00 +0600

    working in alfajores

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x2Bec4B5E67FE9e6Ba5768D83d49a71A60067B813"
+  "0x83C3B38d08F60aa74cF1eA054248447c38aCb634"
 );
 
 export default instance;
